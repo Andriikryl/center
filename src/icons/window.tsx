a62ea475ebc4fd0fcc2eb4e369704541837c4e6a
Andriikryl <andreyk557@gmail.com>
2024-03-28 18:28:17 +0200

    add icons

---
+++ src/icons/window.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function window() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 80">
+      <rect x="0" y="0" rx="50%" width="50" height="80" fill="black"></rect>
+    </svg>
+  );
+}
