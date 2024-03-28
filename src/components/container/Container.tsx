a7054778bfb43506ecd6e885fe3010fa43e54448
Andriikryl <andreyk557@gmail.com>
2024-03-28 17:39:49 +0200

    add Container component

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from "react";
+import styles from "./style.module.css"
+interface ContainerProps {
+    children:ReactNode
+}
+
+export default function Container({ children }:ContainerProps) {
+  return <div className={styles.container}>{children}</div>;
+}
