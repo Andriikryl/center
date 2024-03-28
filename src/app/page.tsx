cca31c9b2453f6c7c3572e9223c9783f25af836b
Andriikryl <andreyk557@gmail.com>
2024-03-28 17:02:18 +0200

    add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "@/components/hero/Hero";
+import CenterFlex from "@/components/centerFlex/CenterFlex";
+import Code from "@/components/code/Code";
+import Container from "@/components/container/Container";
+import CenterGrid from "@/components/centerGrid/CenterGrid";
+import PositionCenter from "@/components/positionCenter/PositionCenter";
+import ViewCenter from "@/components/viewCenter/ViewCenter";
 
 export default function Home() {
   return (
-      <div>ads</div>
+    <>
+      <Hero />
+      <CenterFlex />
+      <Container>
+        {/* <Code
+          code={`.center{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            `}
+          lang="css"
+          theme="github-dark"
+        /> */}
+        <CenterGrid />
+        <PositionCenter />
+        <ViewCenter />
+      </Container>
+    </>
   );
 }
