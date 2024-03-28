53a0f8376032d7ff22db30f7a170be83ec255799
Andriikryl <andreyk557@gmail.com>
2024-03-28 23:07:54 +0200

    fix bug

--- src/hooks/useStickyState.ts
+++ src/hooks/useStickyState.ts
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 
 export function useStickyState<T>(defaultValue:T, key:string) {
     const [value, setValue] = useState(() => {
-      const stickyValue = window.localStorage.getItem(key);
-      return stickyValue !== null
-        ? JSON.parse(stickyValue)
-        : defaultValue;
+      if (typeof window !== 'undefined') {    
+        const stickyValue = window.localStorage.getItem(key);
+        return stickyValue !== null
+          ? JSON.parse(stickyValue)
+          : defaultValue;
+       }
     });
     useEffect(() => {
       window.localStorage.setItem(key, JSON.stringify(value));
